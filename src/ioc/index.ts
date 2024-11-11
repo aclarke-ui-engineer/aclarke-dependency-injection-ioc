2dbb87303bdddd663fe7ade01dae6289b4ac8c4e
Andrew Clarke <aclarke@griddynamics.com>
2024-11-11 09:05:24 -0500

    Prepare ioc container and register resources

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,28 @@
-import IoCContainer from 'ioc-lite';
+import IoCContainer from "ioc-lite";
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from "../services/logger";
+import { HTTP } from "../services/http";
+import { Users } from "../services/users";
+import { ApiConfig } from "src/types";
 
-export const createIoCContainer = () =>  {
-  const ioc = new IoCContainer();
-  // you can register some resources right now below...
+export type ServicesType = {
+  logger: typeof Logger;
+  http: typeof HTTP;
+  users: typeof Users;
+  apiConfig: ApiConfig;
+};
 
+export const createIoCContainer = () => {
+  const config: ApiConfig = (window as any).__CONFIG__;
+  delete (window as any).__CONFIG__;
+
+  const ioc = new IoCContainer<ServicesType>();
+  ioc.registerClass("logger", Logger);
+  ioc.registerClass("http", HTTP);
+  ioc.registerClass("users", Users);
+  ioc.register("apiConfig", config);
   return ioc;
 };
+
+const ioc = createIoCContainer();
+export default ioc;
